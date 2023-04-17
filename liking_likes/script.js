ee3183325068a1ef0504f1b606fdb6923396e7b9
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-04-16 20:46:32 -0400

    final commit 2

--- liking_likes/script.js
+++ liking_likes/script.js
@@ -1,14 +1,14 @@
 var countElement = document.querySelector("#like_button1");
 console.log(countElement);
 
-function add1(){
+function add1() {
     count.innerHTML++;
 }
 
-function add2(){
+function add2() {
     count2.innerHTML++;
 }
 
-function add3(){
+function add3() {
     count3.innerHTML++;
 }
