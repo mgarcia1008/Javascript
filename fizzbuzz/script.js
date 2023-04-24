7603eb64241ba0b1d61f1d8f7170419dcf5e8890
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-04-24 14:08:04 -0400

    added notes about ==0

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,6 +1,7 @@
 
 for (var i = 1; i < 101; i++)
-   if (i % 3 == 0) {
+   // the % operator determines the remainder, if a number is a multiple of 3 it will have no remainder that;s why we put ==0
+    if (i % 3 == 0) {
       console.log("Fizz");
    }
    else if (i % 5 == 0) {
