f2b2c016a49d2e76e35ded68a373115457dc17ae
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-04-24 15:00:07 -0400

    decreasing multiples of 3

--- loop_challenges/script.js
+++ loop_challenges/script.js
@@ -1,3 +1,13 @@
-for(i=0; i<20; i++)
+//print odds 1-20 if divisible by 2 and has a remiander it is odd
+/*for(i=0; i<20; i++)
 if(i % 2 ==1)
-console.log(i);
+console.log(i);*/
+
+//decreasing multiples of 3
+
+for(i=100; i>0; i--){
+    if(i % 3 ==0)
+
+console.log(i)
+}
+
